fbe61bdb0a163850fa59444a674ab994750061af
Mark Jefferson Luces <m.jluces@gmail.com>
2024-05-26 01:48:17 +0800

    Added Buttons

--- djangocrudlesson/urls.py
+++ djangocrudlesson/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('genders', views.index_gender),
     path('gender/create', views.create_gender),
     path('genders_store', views.store_gender),
+    path('gender/show/<int:gender_id>',views.show_gender),
+    path('gender/edit/<int:gender_id>', views.edit_gender),
+    path('gender/update/<int:gender_id>', views.update_gender),
+    path('gender/delete/<int:gender_id>', views.delete_gender),
+    path('gender/destroy/<int:gender_id>', views.destroy_gender),
 ]
--- djangocrudlesson/views.py
+++ djangocrudlesson/views.py
@@ -20,3 +20,45 @@ def store_gender(request):
     Gender.objects.create(gender=gender) #insert into genders(gender) values(gender)
     messages.success(request, 'Gender successfully saved!')
     return redirect('/genders')
+
+def show_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id) #SELECT * FROM genders WHERE gender_id = gender_id
+
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/show.html', context)
+
+def edit_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)
+
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/edit.html', context)
+
+def update_gender(request, gender_id):
+    gender = request.POST.get('gender')
+
+    Gender.objects.filter(pk=gender_id).update(gender=gender) #UPDATE genders SET gender = gender WHERE gender_id = gender_id
+    messages.success(request, 'Gender Successfully Saved')
+
+
+    return redirect("/genders")
+
+def delete_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id)
+
+    context = {
+        'gender': gender,
+    }
+
+    return render(request, 'gender/delete.html', context)
+
+def destroy_gender(request, gender_id):
+    Gender.objects.filter(pk=gender_id).delete() # DELETE FROM genders WHERE gender_id = gender_id
+    messages.success(request, 'Gender Successfully Deleted')
+
+    return redirect('/genders')
